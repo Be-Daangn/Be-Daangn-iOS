a05049b73b24a71f445ea80899d5d0fd91029979
parkikbum <pibum@naver.com>
2021-05-17 18:16:00 +0900

    [feat] 헤더뷰, 탭바 완성

--- Be-Daangn-iOS/Be-Daangn-iOS/Sources/Home/HomeViewController.swift
+++ Be-Daangn-iOS/Be-Daangn-iOS/Sources/Home/HomeViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class HomeViewController: BaseViewController {
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var searchView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setRound()
         
         
         // code
     }
+    
+    func setRound(){
+        headerView.clipsToBounds = true
+        headerView.layer.cornerRadius = 40
+        headerView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner,.layerMaxXMaxYCorner)
+        searchView.clipsToBounds = true
+        searchView.layer.cornerRadius = 40
+        searchView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner,.layerMaxXMaxYCorner)
+        
+    }
 }
+
