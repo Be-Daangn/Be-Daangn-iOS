8dd3c4054f9372bf6cd261b4bf116900c61d0326
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-05-17 23:43:40 +0900

    feat: 상단 뷰 구성+테이블 뷰 배치 + 하단 뷰 모서리 둥글게

--- Be-Daangn-iOS/Be-Daangn-iOS/Sources/News/NewsViewController.swift
+++ Be-Daangn-iOS/Be-Daangn-iOS/Sources/News/NewsViewController.swift
@@ -8,8 +8,17 @@
 import UIKit
 
 class NewsViewController: BaseViewController {
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var middleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 아래 View 모서리 둥글게 하기
+        middleView.layer.cornerRadius = 60
+        middleView.layer.masksToBounds = true
+        middleView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner] // 상단 좌, 우 coner만
         
         // code
     }
