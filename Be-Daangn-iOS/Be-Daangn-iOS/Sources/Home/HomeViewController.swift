505a9a295b184c7081052d967d89167715437894
parkikbum <pibum@naver.com>
2021-05-17 18:54:15 +0900

    [feat] 메인뷰 버튼 구현

--- Be-Daangn-iOS/Be-Daangn-iOS/Sources/Home/HomeViewController.swift
+++ Be-Daangn-iOS/Be-Daangn-iOS/Sources/Home/HomeViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 class HomeViewController: BaseViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var homeTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setRound()
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
+        homeTableView.separatorStyle = .none
         
         
         // code
@@ -21,12 +25,30 @@ class HomeViewController: BaseViewController {
     
     func setRound(){
         headerView.clipsToBounds = true
-        headerView.layer.cornerRadius = 40
+        headerView.layer.cornerRadius = 20
         headerView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner,.layerMaxXMaxYCorner)
         searchView.clipsToBounds = true
-        searchView.layer.cornerRadius = 40
+        searchView.layer.cornerRadius = 20
         searchView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner,.layerMaxXMaxYCorner)
         
     }
+}
+
+extension HomeViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 210
+    }
+}
+extension HomeViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let MainButtonCell = tableView.dequeueReusableCell(withIdentifier: MainButtonTableViewCell.identifier) as? MainButtonTableViewCell else {return UITableViewCell() }
+        return MainButtonCell
+    }
+    
+    
 }
 
