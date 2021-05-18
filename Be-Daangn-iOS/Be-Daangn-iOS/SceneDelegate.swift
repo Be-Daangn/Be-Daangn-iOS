e0f1ee8af1a95c0bfc1fefa2734f06332b20004e
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-05-18 13:57:12 +0900

    feat: 하단탭바 추가

--- Be-Daangn-iOS/Be-Daangn-iOS/SceneDelegate.swift
+++ Be-Daangn-iOS/Be-Daangn-iOS/SceneDelegate.swift
@@ -20,8 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: scene)
         
-        let homeStoryboard = UIStoryboard(name: "NewsStoryboard", bundle: nil)
-        let homeViewController = homeStoryboard.instantiateViewController(identifier: "NewsViewController")
+        let homeStoryboard = UIStoryboard(name: "HomeStoryboard", bundle: nil)
+        let homeViewController = homeStoryboard.instantiateViewController(identifier: "HomeViewController")
         
         self.window?.rootViewController = homeViewController
         
