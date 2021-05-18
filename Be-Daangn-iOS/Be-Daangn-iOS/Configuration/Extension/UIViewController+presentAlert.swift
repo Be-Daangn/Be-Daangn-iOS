b7552467cdac3f2a10bdf0ca010bb8d3910e66ab
Taehyeon-Kim <taehyk528@gmail.com>
2021-05-19 01:26:06 +0900

    feat: makeAlert 익스텐션 추가 (이전거수정)

--- Be-Daangn-iOS/Be-Daangn-iOS/Configuration/Extension/UIViewController+presentAlert.swift
+++ Be-Daangn-iOS/Be-Daangn-iOS/Configuration/Extension/UIViewController+presentAlert.swift
@@ -5,4 +5,50 @@
 //  Created by taehy.k on 2021/05/19.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    // MARK: 취소와 확인이 뜨는 UIAlertController
+    func presentAlertWithCancel(title: String,
+                      message: String? = nil,
+                      okAction: ((UIAlertAction)->Void)?,
+                      cancelAction: ((UIAlertAction)->Void)? = nil,
+                      completion: (() -> Void)? = nil) {
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: cancelAction)
+        alert.addAction(cancelAction)
+
+        self.present(alert, animated: true, completion: completion)
+    }
+    
+    // MARK: 확인이 뜨는 UIAlertController
+    func presentAlert(title: String,
+                      message: String? = nil,
+                      okAction: ((UIAlertAction)->Void)?,
+                      preferredStyle style: UIAlertController.Style = .alert,
+                      completion: (() -> Void)? = nil) {
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: completion)
+        
+    }
+
+
+}
