c4da0549b4f5f33cce48fb9c70d008a00ff3026e
김태현 <taehyk528@gmail.com>
2021-05-31 12:31:41 +0900

    feat: #29 서버 통신 위한 세팅
    - Constants.swift (API URL 추가)
    - NetworkResult Enum 생성

--- Be-Daangn-iOS/Be-Daangn-iOS/Configuration/Constants.swift
+++ Be-Daangn-iOS/Be-Daangn-iOS/Configuration/Constants.swift
@@ -10,5 +10,19 @@
 import Foundation
 
 struct Constants {
-    // code   
+    
+    // MARK: - BASE URL
+    static let baseURL = "http://15.164.251.55:5000/api/main"
+    
+    // MARK: - Feature URL
+    
+    /// 1. 새소식 리스트 URL
+    static let newsURL = baseURL + "/news"
+    
+    /// 2. 추천 리스트 URL
+    static let recommendURL = baseURL + "/recommend"
+    
+    /// 3. 북마크 URL
+    static let bookmarkURL = baseURL + "/recommend/bookmark"
+    
 }
